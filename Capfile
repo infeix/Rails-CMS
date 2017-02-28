e2c814f2fded1a86a5bf74c38ed812b4f555374f
Johannes Kroop <johkroop@gmail.com>
2017-03-01 00:14:44 +0100

    cap and rvm
    Signed-off-by: Johannes Kroop <johkroop@gmail.com>

--- Capfile
+++ Capfile
@@ -1,2 +1,5 @@
 require 'capistrano/rails'
 require 'capistrano/passenger'
+require 'capistrano/rvm'
+set :rvm_type, :user
+set :rvm_ruby_version, '2.3.1'
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,8 @@
 # config valid only for current versiorequire 'capistrano/ext/multistage'n of Capistrano
 set :application, 'Rails-CMS'
 set :repo_url, 'git@github.com:infeix/Rails-CMS.git'
-set :ruby_version, '2.3.1'
 
 set :deploy_to, '/home/deploy/Rails-CMS'
-
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-set :pty, false
 
 append :linked_files, "config/database.yml", "config/secrets.yml"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
