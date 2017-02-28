ad955ba12f5160dcb12dc1286f9c369eece98cfb
Johannes Kroop <johkroop@gmail.com>
2017-02-28 23:56:28 +0100

    multi stages
    Signed-off-by: Johannes Kroop <johkroop@gmail.com>

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,14 @@
-# config valid only for current version of Capistrano
-lock '3.6.1'
-
+# config valid only for current versiorequire 'capistrano/ext/multistage'n of Capistrano
 set :application, 'Rails-CMS'
 set :repo_url, 'git@github.com:infeix/Rails-CMS.git'
 set :ruby_version, '2.3.1'
 
-set :bundle_bins, %w(rake ruby rails)
+set :deploy_to, '/home/deploy/Rails-CMS'
+
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 set :pty, false
 
-set :linked_dirs, fetch(:linked_dirs, []).push('storage')
-set :user, "deploy"
+append :linked_files, "config/database.yml", "config/secrets.yml"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
 
