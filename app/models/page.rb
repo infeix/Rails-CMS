d81653c95e9aaf323d0c109f461a8cd8fe4b7607
Johannes Kroop <johkroop@gmail.com>
2017-03-02 21:47:25 +0100

    mvc
    Signed-off-by: Johannes Kroop <johkroop@gmail.com>

--- app/models/page.rb
+++ app/models/page.rb
@@ -10,7 +10,15 @@ class Page < ActiveRecord::Base
   end
 
   def render
-    template.present? ? template.render(articles.all) : text
+    template.present? ? template.render(articles.all) : render_articles(articles.all)
+  end
+
+  def render_articles(arts)
+    html = ''
+    arts.each do |article|
+      html += article.to_s
+    end
+    html
   end
 
   def to_s
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,74 @@
-# frozen_string_literal: true
-
-# This file should contain all the record creation needed to seed the database with its default
-# values. The data can then be loaded with the rake db:seed (or created alongside the db with
-# db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
 def debug(message)
   puts message unless Rails.env.test?
 end
 
 debug 'Creating template'
-temp = Template.find_or_create_by(name: 'johanneskroop.de')
-temp.html_begin = '<h1>'
-temp.html_end = '</h1>'
+temp = Template.find_or_create_by(title: 'test')
 temp.save!
 
-debug 'Creating aricles'
-index = Article.find_or_create_by(path: 'index')
-index.text = 'Hello World!'
+css1 = CssPart.find_or_create_by(index: 1, template_id: temp.id)
+css1.title = 'test_css'
+css1.text = '
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+label {
+    color: red;
+}
+'
+css1.save!
+
+part1 = HtmlPart.find_or_create_by(index: 1, template_id: temp.id)
+part1.text = '
+    <h1 >'
+part1.save!
+
+# Part 2
+part2 = HtmlPart.find_or_create_by(index: 2, template_id: temp.id)
+part2.text = '
+    </h1>
+'
+part2.save!
+
+# Part 3
+
+debug 'Creating index page'
+index = Page.find_or_create_by(path: 'index')
+index.title = 'Start'
 index.template = temp
 index.save!
+
+art1 = Article.find_or_create_by(index: 1, page_id: index.id)
+art1.text = '
+Hallo CMS!
+'
+art1.save!
+
+debug 'Creating Impressum page'
+impressum = Page.find_or_create_by(path: 'impressum')
+impressum.title = 'Impressum'
+impressum.template = nil
+impressum.save!
+
+art_impressum = Article.find_or_create_by(index: 1, page_id: impressum.id)
+art_impressum.text = '
+<h1>Impressum</h1>
+<p>
+  <br>
+  Name<br>
+  Straße<br>
+  Stadt<br>
+  UST-ID: ****<br>
+  <br>
+  E-Mail: <a href="mailto:test@test.de">test@test.de</a><br>
+  <br>
+</p>'
+art_impressum.save!
+
+user = User.find_or_create_by(is_admin: true, email: 'test@test.de')
+user.password = 'secret42'
+user.password_confirmation = 'secret42'
+user.save!
