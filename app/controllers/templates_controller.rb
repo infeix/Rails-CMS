5f5fa1e409a03764e9aea43bf33129a00262b973
Johannes Kroop <johkroop@gmail.com>
2017-03-05 11:14:32 +0100

    repair edit template
    Signed-off-by: Johannes Kroop <johkroop@gmail.com>

--- app/controllers/templates_controller.rb
+++ app/controllers/templates_controller.rb
@@ -74,6 +74,6 @@ class TemplatesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def template_params
-      params.require(:template).permit(:name, :meta)
+      params.require(:template).permit(:title, :meta)
     end
 end
