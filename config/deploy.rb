2682545dcb9f63daaabe6a9becae19dd350fd446
Johannes Kroop <johkroop@gmail.com>
2017-03-07 22:46:07 +0100

    images link
    Signed-off-by: Johannes Kroop <johkroop@gmail.com>

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,4 +8,4 @@ set :ruby_version, '2.3.1'
 set :deploy_to, '/home/deploy/Rails-CMS'
 
 append :linked_files, "config/database.yml", "config/secrets.yml", "config/initializers/devise.rb"
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads", "public/images"
