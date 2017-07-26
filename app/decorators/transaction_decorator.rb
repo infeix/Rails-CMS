8e412b958796c40540dd105d2549e50ec1821ad6
Johannes Kroop <johkroop@gmail.com>
2017-07-26 21:34:27 +0200

    debug show account

--- app/decorators/transaction_decorator.rb
+++ app/decorators/transaction_decorator.rb
@@ -2,9 +2,12 @@
 class TransactionDecorator < Draper::Decorator
   delegate_all
 
-  def render_in_row(row_index, to)
+  def render_in_account(account, rows)
+    row_index = rows[other_account(account).id]
+    to = to?(account)
     before = '<div class="transaction-row"></div>' * (row_index - 1)
-    "<div class=\"transaction-row\">#{invoice_date}</div>#{before}<div class=\"transaction-row\">#{sign(to)}#{total}</div><div class=\"float-reset\"></div>"
+    after = '<div class="transaction-row"></div>' * (rows.count - row_index)
+    "<div class=\"transaction-row\">#{name}</div><div class=\"transaction-row\">#{invoice_date}</div>#{before}<div class=\"transaction-row\">#{sign(to)}#{total}</div>#{after}<div class=\"float-reset\"></div>"
   end
 
   def sign(to)
