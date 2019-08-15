168c3085d0a81ddf55af435de0128359b5fcebbc
Johannes Kroop <johkroop@gmail.com>
2019-08-15 12:21:35 +0200

    small template also for links

--- app/models/urlelement.rb
+++ app/models/urlelement.rb
@@ -10,6 +10,12 @@ class Urlelement < Article
         template_element.html_parts[2].to_s +
         "</a>" +
         template_element.html_parts[3].to_s.html_safe
+    elsif template_element && template_element.html_parts.all.count > 1 then
+      template_element.html_parts[0].to_s +
+        "<a href=\"#{target_path}\" data-target=\"#{target_path}\" data-position=\"#{position}\" #{template_element.meta} #{data_text}>" +
+        text +
+        "</a>" +
+        template_element.html_parts[1].to_s
     else
       "<a href=\"#{target_path}\" #{data_text}>#{text}</a>".html_safe
     end
