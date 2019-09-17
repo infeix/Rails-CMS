eebcddb3658854b365b80012f5813ee8220294c2
Johannes Kroop <johkroop@gmail.com>
2019-09-18 00:22:47 +0200

    staging mod

--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,5 @@
+ENV["RAILS_ENV"] = "staging" if File.exists?(File.expand_path('staging.txt', __dir__))
+Rails.env = "staging" if File.exists?(File.expand_path('staging.txt', __dir__))
 # Load the Rails application.
 require_relative 'application'
 
