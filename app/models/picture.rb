17f57229462ea3740a25cc47e33832e476939fd3
Johannes Kroop <johkroop@gmail.com>
2019-09-19 15:49:15 +0200

    template for pictures

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -6,6 +6,12 @@ class Picture < ContentPart
   end
 
   def to_s
-    "<img src=\"#{image.url}\" alt=\"Image\">"
+    if template_element && template_element.html_parts.all.count > 1 then
+      template_element.html_parts[0].to_s +
+        "<img src=\"#{image.url}\" alt=\"Image\">" +
+        template_element.html_parts[1].to_s
+    else
+      "<img src=\"#{image.url}\" alt=\"Image\">"
+    end
   end
 end
