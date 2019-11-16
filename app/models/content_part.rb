24979fdf2797277d7add479c2d87f261bbed2f4b
Johannes Kroop <johkroop@gmail.com>
2019-11-16 17:09:53 +0100

    easy editing

--- app/models/content_part.rb
+++ app/models/content_part.rb
@@ -11,7 +11,12 @@ class ContentPart < ActiveRecord::Base
   has_and_belongs_to_many :pages, optional: true
   scope :sort_by_index, -> { order(index: :asc) }
 
+  before_save :create_position
   after_save :create_positions
+
+  def create_position
+    self.position = title if self.position.eql? "no_position"
+  end
 
   def create_positions
     Position.create_positions to_s
