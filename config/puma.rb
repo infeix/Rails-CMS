90a2d7e408adea966bb4736e4009d93e37c5f0e3
Johannes Kroop <johkroop@gmail.com>
2020-02-25 01:50:07 +0100

    updates

--- config/puma.rb
+++ config/puma.rb
@@ -1,8 +1,8 @@
 # Change to match your CPU core count
-workers 2
+workers 1
 
 # Min and Max threads per worker
-threads 1, 2
+threads 1, 1
 
 app_dir = File.expand_path("../..", __FILE__)
 shared_dir = "#{app_dir}/shared"
@@ -18,12 +18,15 @@ bind "unix://#{shared_dir}/tmp/sockets/Rails-CMS-puma.sock"
 stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
 
 # Set master PID and state locations
-pidfile "#{shared_dir}/pids/puma.pid"
-state_path "#{shared_dir}/pids/puma.state"
+pidfile "#{shared_dir}/tmp/pids/puma.pid"
+state_path "#{shared_dir}/tmp/pids/puma.state"
 activate_control_app
 
 on_worker_boot do
-  require "active_record"
-  ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
-  ActiveRecord::Base.establish_connection(YAML.load_file("#{app_dir}/config/database.yml")[rails_env])
+  # worker specific setup
+  ActiveSupport.on_load(:active_record) do
+    require "active_record"
+    ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
+    ActiveRecord::Base.establish_connection(YAML.load_file("#{app_dir}/config/database.yml")[rails_env])
+  end
 end
