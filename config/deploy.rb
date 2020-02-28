ff094a7f8e6a7f043088f1052f037164039de33f
Johannes Kroop <johkroop@gmail.com>
2020-02-28 12:06:46 +0100

    fix configuration errors

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,6 +3,6 @@
 set :repo_url, 'git@github.com:infeix/Rails-CMS.git'
 set :ruby_version, '2.5.7'
 
-append :linked_files, "config/database.yml"
+append :linked_files, "config/database.yml", "config/secrets.yml", "config/initializers/devise.rb"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads", "public/images"
 
