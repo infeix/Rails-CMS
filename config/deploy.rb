acafd4bc3e40380d59963d811c7c0c97de4fe73c
Johannes Kroop <johkroop@gmail.com>
2020-02-28 07:38:23 +0100

    config

---
+++ config/deploy.rb
@@ -0,0 +1,8 @@
+
+
+set :repo_url, 'git@github.com:infeix/Rails-CMS.git'
+set :ruby_version, '2.3.1'
+
+append :linked_files, "config/database.yml", "config/secrets.yml", "config/initializers/devise.rb"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads", "public/images"
+
--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
+ENV["RAILS_ENV"] = "staging" if File.exists?(File.expand_path('staging.txt', __dir__))
+
 # Load the Rails application.
 require_relative 'application'
+Rails.env = "staging" if File.exists?(File.expand_path('staging.txt', __dir__))
 
 # Initialize the Rails application.
 Rails.application.initialize!
