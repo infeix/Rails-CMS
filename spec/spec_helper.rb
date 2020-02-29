50051feb575a1b8dcc2ca6781b94f8029fcde7d2
Johannes Kroop <johkroop@gmail.com>
2020-02-29 15:11:04 +0100

    updated gems

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,8 +9,6 @@ if ENV['CIRCLE_ARTIFACTS']
 end
 SimpleCov.start 'rails'
 
-require 'rspec-pretty_fail_formatter'
-
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
@@ -46,6 +44,4 @@ RSpec.configure do |config|
   # test failures related to randomization by passing the same `--seed` value
   # as the one that triggered the failure.
   Kernel.srand config.seed
-
-  config.add_formatter PrettyFailFormatter
 end
