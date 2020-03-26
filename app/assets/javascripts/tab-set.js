05c9250a504c7a10d9141e507d1b6882715a9a94
Johannes Kroop <johkroop@gmail.com>
2020-03-26 17:24:30 +0100

    js tab fix

--- app/assets/javascripts/tab-set.js
+++ app/assets/javascripts/tab-set.js
@@ -2,16 +2,19 @@ function openTab(evt, name) {
     // Declare all variables
     var i, tabcontent, tablinks;
 
-    // Get all elements with class="tabcontent" and hide them
-    $(".selected-tab").each(function(index) {
-      $(this).removeClass('selected-tab')
-    })
-    $(".tablink").each(function(index) {
-      $(this).removeClass('pure-button-primary')
-    })
-    $("." + name + "-tab").addClass('selected-tab')
-    $("." + name + "-tablink").addClass('pure-button-primary')
-    window.location.hash = "#" + name;
+    if($("." + name + "-tab").text().length > 0 && $("." + name + "-tablink").text().length > 0)
+    {
+        // Get all elements with class="tabcontent" and hide them
+        $(".selected-tab").each(function(index) {
+          $(this).removeClass('selected-tab')
+        })
+        $(".tablink").each(function(index) {
+          $(this).removeClass('pure-button-primary')
+        })
+        $("." + name + "-tab").addClass('selected-tab')
+        $("." + name + "-tablink").addClass('pure-button-primary')
+        window.location.hash = "#" + name;
+    }
 }
 
 $( document ).ready(function() {
